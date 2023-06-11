e0999b4bf5731011f167101db09df7135b08e052
nazmunnaharnijhum <nazmunnaharnijhum@gmail.com>
2023-06-11 13:16:41 +0600

    set api to update a user role as an instructor

--- index.js
+++ index.js
@@ -50,12 +50,26 @@ async function run() {
         res.send(result);
     })
 
-    app.patch('users/admin:id', async(req, res) => {
+    app.patch('/users/admin/:id', async(req, res) => {
         const id = req.params.id;
         const filter = {_id: new ObjectId(id)};
         const updateDoc = {
             $set: {
                 role: 'admin'
+            },
+        };
+
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+
+    })
+
+    app.patch('/users/instructor/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const updateDoc = {
+            $set: {
+                role: 'instructor'
             },
         };
 

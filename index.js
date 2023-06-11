cf8e631c255552f3cbd38d39ca83ed4e21a451d3
nazmunnaharnijhum <nazmunnaharnijhum@gmail.com>
2023-06-11 10:47:53 +0600

    set api to get users

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ async function run() {
     const cartCollection = client.db("summerDb").collection("carts");
 
     //users related apis
+    app.get('/users', async (req, res) =>{
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+    })
+
     app.post('/users', async(req, res) => {
         const user = req.body;
         console.log(user);
